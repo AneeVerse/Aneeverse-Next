953b61e8caef0a018e33fbb04dc8ff21d41146a6
Saood Ahmad <99206626+saoodchoudhary@users.noreply.github.com>
2025-03-05 17:27:32 +0530

    update

--- src/___BlogData/designBlog/index/designIndex.js
+++ src/___BlogData/designBlog/index/designIndex.js
@@ -2,4 +2,10 @@ import { blog1 } from "../blog1";
 
 
 
-export const designBlog = [...blog1 ];
+export const designBlog = [
+    ...blog1,
+    //...blog2,
+    //...blog3,
+    //...blog4,
+    //...blog5,
+];
