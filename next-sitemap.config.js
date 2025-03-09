b788677b9fd562e1237b017c03f5d61c631fd0ec
Saood Ahmad <saoodchoudhary9@gmail.com>
2025-03-09 15:45:24 +0530

    update

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-
-module.exports = {
-    siteUrl: "https://www.aneeverse.com", // Apni website ka URL yahan daalo
-    generateRobotsTxt: true, // robots.txt bhi generate karega
-    sitemapSize: 5000, // Ek sitemap file mein max URLs
-  };
-  
