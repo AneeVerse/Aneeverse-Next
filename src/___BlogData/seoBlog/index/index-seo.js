dae0346985d892a01cd7e154078c97e244fe437b
Aneeverse <83137173+AneeVerse@users.noreply.github.com>
2025-03-12 11:38:57 +0530

    new name

--- src/___BlogData/seoBlog/index/index-seo.js
+++ src/___BlogData/seoBlog/index/index-seo.js
@@ -1,12 +1,11 @@
-import { how_content_marketing_helps_your_business_grow1 } from "../how_content_marketing_helps_your_business_grow1";
 import { temp_content_marketing_growth } from "../temp_content_marketing_growth";
-
+import { how_content_marketing_helps_your_business_growa } from "../how_content_marketing_helps_your_business_growa";
 
 
 
 export const SeoBlog = [
     temp_content_marketing_growth,
-    how_content_marketing_helps_your_business_grow1,
+    how_content_marketing_helps_your_business_growa,
     //blog3,
     //blog4,
     //blog5,
