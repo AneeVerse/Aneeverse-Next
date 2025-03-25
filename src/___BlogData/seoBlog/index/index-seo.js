dbeef53f2854cc2e6d2054678be54c4cf21d69b5
mayur5689 <mayur123roger@gmail.com>
2025-03-25 17:57:32 +0530

    Add SEO strategies for small businesses with accompanying images

--- src/___BlogData/seoBlog/index/index-seo.js
+++ src/___BlogData/seoBlog/index/index-seo.js
@@ -1,10 +1,12 @@
 import { how_content_marketing_helps_your_business_grow } from "../how_content_marketing_helps";
 import { temp_content_marketing_growth } from "../temp_content_marketing_growth";
+import { seo_strategies_small_business } from "../seo_strategies_small_business";
 
 
 
 
 export const SeoBlog = [
+    seo_strategies_small_business,
     temp_content_marketing_growth,
     how_content_marketing_helps_your_business_grow,
     //blog3,
