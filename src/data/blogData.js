54213894ca0e19c78bfc419370ad08a4bdb910bb
mayur5689 <mayur123roger@gmail.com>
2025-03-27 12:33:14 +0530

    Add new SEO blog section with three posts on On-Page SEO, E-commerce Product Pages SEO, and the importance of Blogging for SEO, including images and integration into the main blog index

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,7 +1,7 @@
 import { designBlog } from "@/___BlogData/designBlog/index/index-design";
 import { ebayBlog } from "@/___BlogData/ebayBlog/index/index-ebay";
 import { ecommerceBlog } from "@/___BlogData/ecommerceBlog/index/index-ecommerce";
-import { SeoBlog } from "@/___BlogData/seoBlog/index/index-seo";
+import { SeoBlog } from "@/___BlogData/seoBlog";
 import { UiuxBlog } from "@/___BlogData/uiuxBlog/index-uiux";
 import { LocalseoBlog } from "@/___BlogData/localseoBlog/index-localseo";
 import { contentMarketingBlogPosts } from "@/___BlogData/contentMarketingBlog";
