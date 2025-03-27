b117a0a139f394232c1fed1c27b502af6ea39467
mayur5689 <mayur123roger@gmail.com>
2025-03-27 10:14:57 +0530

    Add new E-commerce blog post on improving website sales, including content, images, and integration into the blog index

--- src/___BlogData/ecommerceBlog/index/index-ecommerce.js
+++ src/___BlogData/ecommerceBlog/index/index-ecommerce.js
@@ -1,10 +1,9 @@
 import { temp_ui_trends_2025 } from "../temp_ui_trends_2025";
-
-
+import { improve_ecommerce_sales } from "../improve_ecommerce_sales";
 
 export const ecommerceBlog = [
+    improve_ecommerce_sales,
     temp_ui_trends_2025,
-    //blog2,
     //blog3,
     //blog4,
     //blog5,
