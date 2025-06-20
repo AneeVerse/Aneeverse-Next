9e727dce2e161296e4650dcd58aa406f66f6ebef
mayur5689 <your-email@example.com>
2025-06-20 14:54:41 +0530

    Update BlogCard Component Aspect Ratio for Image Rendering
    - Changed the aspect ratio of the image container in the BlogCard component from 16:9 to 16:11, improving the visual presentation of blog thumbnails.

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -38,7 +38,7 @@ export default function BlogCard({ blog }) {
     <div className="flex flex-col h-full">
       {/* Image Section - Superside style */}
       <Link href={`/blog/${slug}`} className="block group overflow-hidden rounded-lg">
-        <div className="relative w-full aspect-[16/9] overflow-hidden">
+        <div className="relative w-full aspect-[16/11] overflow-hidden">
           <SanityImage
             image={blog?.thumbnail}
             alt={title}
