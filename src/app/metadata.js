797d8b1287b74544120b841b80722def6e387657
mayur5689 <your-email@example.com>
2025-07-09 11:14:36 +0530

    Add SEO Metadata for Homepage
    - Introduced default SEO metadata for the homepage, including title, description, and Open Graph and Twitter card details.
    - Enhanced the metadata structure to improve search engine visibility and social media sharing capabilities.
    - Ensured consistency in branding and messaging across different platforms.

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,1 +1,30 @@
- 
+export const metadata = {
+  title: {
+    default: "Aneeverse: Your creative team's growth partner",
+    template: "%s | Aneeverse",
+  },
+  description:
+    "Aneeverse is your dedicated, on-call creative & digital marketing team. Design, optimise and advertise your brand—ship campaigns faster, more reliably and at scale.",
+  openGraph: {
+    title: "Aneeverse: Your creative team's growth partner",
+    description:
+      "Aneeverse is your dedicated, on-call creative & digital marketing team. Design, optimise and advertise your brand—ship campaigns faster, more reliably and at scale.",
+    url: `https://aneeverse.com/`,
+    images: [
+      {
+        url: "/images/meta/phone.avif",
+        width: 1200,
+        height: 630,
+        alt: "Aneeverse: Your creative team's growth partner",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Aneeverse: Your creative team's growth partner",
+    description:
+      "Aneeverse is your dedicated, on-call creative & digital marketing team. Design, optimise and advertise your brand—ship campaigns faster, more reliably and at scale.",
+    image: "/images/meta/phone.avif",
+  },
+}; 
