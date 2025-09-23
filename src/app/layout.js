d6174f84930833fcd43b8e5324d8f91ff0ae9428
mayur5689 <83137173+AneeVerse@users.noreply.github.com>
2025-09-23 13:23:02 +0530

    lead finder 02

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,8 +31,8 @@ export default function RootLayout({ children }) {
         </Script>
 
 
-
-            <script> (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('p1e024BX9mK8GB6d'); </script>
+<script> (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('p1e024BX9mK8GB6d'); </script>
+            
 
 
         {/* Google Analytics gtag.js */}
