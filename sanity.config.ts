8d9c2bea72489f406edd26b326bfa91364509678
mayur5689 <mayur123roger@gmail.com>
2026-01-13 14:39:17 +0530

    implemented table copy paste

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,19 +4,20 @@
  * This configuration is used to for the Sanity Studio that's mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { markdownSchema } from 'sanity-plugin-markdown'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
-import {customComponents} from './src/sanity/components'
-import {pastePlugin} from './src/sanity/plugins/pastePlugin'
-import {directPastePlugin} from './src/sanity/plugins/directPastePlugin'
-import {faqDashboardPlugin} from './src/sanity/plugins/faqDashboardPlugin'
-import {mainImageBodySyncPlugin} from './src/sanity/plugins/mainImageBodySyncPlugin'
+import { apiVersion, dataset, projectId } from './src/sanity/env'
+import { schema } from './src/sanity/schemaTypes'
+import { structure } from './src/sanity/structure'
+import { customComponents } from './src/sanity/components'
+import { pastePlugin } from './src/sanity/plugins/pastePlugin'
+import { directPastePlugin } from './src/sanity/plugins/directPastePlugin'
+import { faqDashboardPlugin } from './src/sanity/plugins/faqDashboardPlugin'
+import { mainImageBodySyncPlugin } from './src/sanity/plugins/mainImageBodySyncPlugin'
 
 export default defineConfig({
   basePath: '/studio',
@@ -25,10 +26,12 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
+    // Markdown support
+    markdownSchema(),
     // Custom components including the table editor
     customComponents,
     // Plugin for handling paste operations
